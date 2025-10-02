58146cd0522e0dfee33b561783919666684525d2
Jyrki Alakuijala <jyrki@google.com>
2025-10-02 17:20:23 +0200

    copying current identity-fft approach to revolve
    should have more separation of instruments, didn't listen yet
    
    8th order filtering vs. 3rd order in the past

--- scripts/upmix.py
+++ scripts/upmix.py
@@ -41,16 +41,16 @@ run("./build/revolve /tmp/down-dry.wav /tmp/16speakers.wav /tmp/stereo2.wav"); #
 
 run("sox /tmp/stereo2.wav /tmp/stereo3.wav norm -2; play /tmp/stereo3.wav ");
 
-#run("sox /tmp/16speakers.wav -e float -b 32 /tmp/stereo.wav remix 1,2,3,4,5,6,7,8 9,10,11,12,13,14,15,16 norm -22")
+run("sox /tmp/16speakers.wav -e float -b 32 /tmp/stex.wav remix 1,2,3,4,5,6,7,8 9,10,11,12,13,14,15,16 norm -22")
 mix0 = ",".join(["%d,v%g" % (i + 1, (i + 1) / 256.0) for i in range(16)])
 mix1 = ",".join(["%d,v%g" % (i + 1, (16 - i) / 256.0) for i in range(16)])
 run("sox /tmp/16speakers.wav -e float -b 32 /tmp/stereo.wav remix " + mix1 + " " + mix0 + " norm -22")
 
 run("sox /tmp/16speakers.wav -b 24 /tmp/down3-norm.wav remix 0 0 1v1 2v1 3v1 4v1 5v1 6v1 7v1 8v1 0 0 9v1.0 10v1.0 11v1.0 12v1.0 13v1.0 14v1.0 15v1.0 16v1.0 norm -32")
 
-#run("amixer --card 2 cset numid=3,iface=MIXER,name='UMC1820 Output Playback Volume' 127,127,127,127,127,127,127,127,121,121,121,121,121,121,121,121")
-run("amixer --card 2 cset numid=3,iface=MIXER,name='UMC1820 Output Playback Volume' 127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127")
-run("amixer --card 2 cset numid=1,iface=MIXER,name='UMC1820 Output Playback Switch' on,on,on,on,on,on,on,on,on,on,on,on,on,on,on,on")
+#run("amixer --card 3 cset numid=3,iface=MIXER,name='UMC1820 Output Playback Volume' 127,127,127,127,127,127,127,127,121,121,121,121,121,121,121,121")
+run("amixer --card 3 cset numid=3,iface=MIXER,name='UMC1820 Output Playback Volume' 127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127")
+run("amixer --card 3 cset numid=1,iface=MIXER,name='UMC1820 Output Playback Switch' on,on,on,on,on,on,on,on,on,on,on,on,on,on,on,on")
 
 run("aplay -D 'hw:CARD=UMC1820,DEV=0' /tmp/down3-norm.wav");
 
--- speaker_experiments/revolve.cc
+++ speaker_experiments/revolve.cc
@@ -48,43 +48,32 @@ float ActualLeftToRightRatio(float left, float right) {
   return std::sqrt((1e-13 + left) / (1e-13 + right));
 }
 
-// Fast approximate function for temporal coherence of 3x exp leakers.
-float FindMedian3xLeaker(float window) { return -2.32 / log(window); }
-
 constexpr int64_t kNumRotators = 128;
 
-float GetFilterGains(int i) {
-  static const float kFilterGains[kNumRotators] = {
-      1.050645, 1.948438, 3.050339, 3.967913, 4.818584, 5.303335, 5.560281,
-      5.490826, 5.156689, 4.547374, 3.691308, 2.666868, 1.539254, 0.656948,
-      0.345893, 0.327111, 0.985318, 1.223506, 0.447645, 0.830961, 1.075181,
-      0.613335, 0.902695, 0.855391, 0.817774, 0.823359, 0.841483, 0.838562,
-      0.831912, 0.808731, 0.865214, 0.808036, 0.850837, 0.821305, 0.839458,
-      0.829195, 0.836373, 0.827271, 0.836018, 0.834514, 0.825624, 0.836999,
-      0.833990, 0.832992, 0.830897, 0.832593, 0.846116, 0.824796, 0.829331,
-      0.844509, 0.838830, 0.821733, 0.840738, 0.841735, 0.827570, 0.838581,
-      0.837742, 0.834965, 0.842970, 0.832145, 0.847596, 0.840942, 0.830891,
-      0.850632, 0.841468, 0.838383, 0.841493, 0.855118, 0.826750, 0.848000,
-      0.874356, 0.812177, 0.849037, 0.893550, 0.832527, 0.827986, 0.877198,
-      0.851760, 0.846317, 0.883044, 0.843178, 0.856925, 0.857045, 0.860695,
-      0.894345, 0.870391, 0.839519, 0.870541, 0.870573, 0.902951, 0.871798,
-      0.818328, 0.871413, 0.921101, 0.863915, 0.793014, 0.936519, 0.888107,
-      0.856968, 0.821018, 0.987345, 0.904846, 0.783447, 0.973613, 0.903628,
-      0.875688, 0.931024, 0.992087, 0.806914, 1.050332, 0.942569, 0.800870,
-      1.210426, 0.916555, 0.817352, 1.126946, 0.985119, 0.922530, 0.994633,
-      0.959602, 0.381419, 1.879201, 2.078451, 0.475196, 0.952731, 1.709305,
-      1.383894, 1.557669,
+float Freq(int i) {
+  // Center frequencies of the filter bank, plus one frequency in both ends.
+  static const float kFreq[130] = {
+    18.949270513107507, 20.0, 21.10899201757206, 22.279477199896046, 23.51486531841422, 24.818755215034393, 26.194945286011798, 27.647444547157992, 29.180484312611213, 30.798530521189843, 32.50629674623729, 34.30875792685758, 36.21116486054244, 38.219059499408814, 40.33829109460661, 42.57503323592745, 44.93580183625289, 47.4274741132331, 50.05730862349216, 52.83296640772185, 55.76253330826266, 58.85454352418564, 62.1180044724941, 65.56242302786917, 69.19783321739865, 73.03482545096755, 77.08457737246212, 81.35888642166094, 85.87020420166964, 90.63167275201636, 95.65716283307582, 100.9613143333494, 106.55957891731282, 112.46826503806983, 118.70458544593973, 125.2867073313771, 132.2338052482963, 139.56611697197326, 147.3050024542456, 155.47300604775518, 164.09392218049982, 173.1928646720131, 182.79633989309812, 192.93232398223984, 203.63034434363638, 214.92156566426348, 226.83888070055136, 239.41700609914596, 252.6925835308937, 266.7042864326648, 281.49293266796866, 297.10160343455505, 313.57576876539406, 330.9634199886363, 349.31520953242364, 368.68459848182215, 389.1280123177271, 410.7050052914292, 433.4784339136835, 457.5146400636791, 482.88364425132784, 509.65934959586934, 537.9197571150087, 567.747192951751, 599.2285482008728, 632.4555320336758, 667.5249388584077, 704.5389302946203, 743.605332782895, 784.8379516969072, 828.356902872881, 874.2889625222178, 922.7679365466429, 973.9350503317262, 1027.9393601543027, 1084.9381874022652, 1145.0975768716758, 1208.5927804762655, 1275.6087677784355, 1346.3407648289965, 1420.9948228853573, 1499.7884186649117, 1582.951087882232, 1670.7250939156525, 1763.3661335511417, 1861.1440818593978, 1964.3437783760758, 2073.265856875396, 2188.2276211543713, 2309.563969378916, 2437.6283696845794, 2572.793889873949, 2715.454284210368, 2866.0251404739256, 3024.945090621247, 3192.677088575884, 3369.7097588716774, 3556.5588200778457, 3753.7685871524372, 3961.9135571006773, 4181.6000825574365, 4413.46813816918, 4658.193184921091, 4916.488137840395, 5189.105442808031, 5476.839268528722, 5780.5278200449, 6101.055780534051, 6439.356888502758, 6796.416657885119, 7173.275248969538, 7571.030498517261, 7990.841117899773, 8433.930068571646, 8901.58812471199, 9395.177633412985, 9916.136483369311, 10465.982293629893, 11046.316834614197, 11658.830694272148, 12305.308202980745, 12987.632631524228, 13707.791677300165, 14467.883254733495, 15270.12160676669, 16116.843755229636, 17010.516308879927, 17953.742648946285, 18949.27051310751, 20000.0, 
+
   };
-  return kFilterGains[i];
+  return kFreq[i + 1];
+}
+
+float FreqAve(int i) {
+  return sqrt(Freq(i - 1) * Freq(i + 1));
+}
+
+double CalculateBandwidthInHz(int i) {
+  return 0.5 * (Freq(i + 1) - Freq(i - 1));
 }
 
 struct PerChannel {
   // [0..1] is for real and imag of 1st leaking accumulation
   // [2..3] is for real and imag of 2nd leaking accumulation
   // [4..5] is for real and imag of 3rd leaking accumulation
-  float accu[6][kNumRotators] = {0};
+  double accu[16][kNumRotators] = {0};
   float LenSqr(size_t i) {
-    return accu[4][i] * accu[4][i] + accu[5][i] * accu[5][i];
+    return accu[14][i] * accu[14][i] + accu[15][i] * accu[15][i];
   }
 };
 
@@ -110,68 +99,57 @@ struct Rotators {
     // values. Recordings can sound better with -2.32 as it pushes the bass
     // signals a bit earlier and likely compensates human hearing's deficiency
     // for temporal separation.
-    const float kMagic = -2.2028003503591482;
-    const float kAlmostHalfForRounding = 0.4687;
-    return static_cast<int>(kMagic / log(window) + kAlmostHalfForRounding);
+    static const double kMagic = 5.46;
+    return kMagic / window;
   }
 
   Rotators() {}
-  Rotators(int num_channels, std::vector<float> frequency,
-           std::vector<float> filter_gains, const float sample_rate) {
+  Rotators(int num_channels, const float sample_rate) {
     channel.resize(num_channels);
+    static const float kSampleRate = 48000.0;
+    static const float kHzToRad = 2.0f * M_PI / kSampleRate;
+    static const double kWindow = 0.99964;
     for (int i = 0; i < kNumRotators; ++i) {
-      // The parameter relates to the frequency shape overlap and window length
-      // of triple leaking integrator.
-      float kWindow = 0.9996;
-      float w40Hz = std::pow(kWindow, 128.0 / kNumRotators);  // at 40 Hz.
-      window[i] = pow(w40Hz, std::max(1.0, frequency[i] / 40.0));
-      delay[i] = FindMedian3xLeaker(window[i]);
+      float bandwidth = CalculateBandwidthInHz(i);  // bandwidth per bucket.
+      window[i] = std::pow(kWindow, bandwidth);
+      delay[i] = FindMedian3xLeaker(1.0 - window[i]);
       float windowM1 = 1.0f - window[i];
       max_delay_ = std::max(max_delay_, delay[i]);
-      float f = frequency[i] * 2.0f * M_PI / sample_rate;
-      gain[i] = filter_gains[i] * pow(windowM1, 3.0);
+      const float f = FreqAve(i) * kHzToRad;
+      gain[i] = pow(windowM1, 4.0);
       rot[0][i] = float(std::cos(f));
       rot[1][i] = float(-std::sin(f));
-      rot[2][i] = sqrt(gain[i]);
+      rot[2][i] = gain[i];
       rot[3][i] = 0.0f;
     }
     for (size_t i = 0; i < kNumRotators; ++i) {
-      advance[i] = max_delay_ - delay[i];
+      advance[i] = 1 + max_delay_ - delay[i];
     }
   }
   void AddAudio(int c, int i, float audio) {
-    audio *= 0.03;
+    //    audio *= 0.03;
     channel[c].accu[0][i] += rot[2][i] * audio;
     channel[c].accu[1][i] += rot[3][i] * audio;
   }
   void OccasionallyRenormalize() {
     for (int i = 0; i < kNumRotators; ++i) {
       float norm =
-          sqrt(gain[i] / (rot[2][i] * rot[2][i] + rot[3][i] * rot[3][i]));
+	gain[i] / sqrt(rot[2][i] * rot[2][i] + rot[3][i] * rot[3][i]);
       rot[2][i] *= norm;
       rot[3][i] *= norm;
     }
   }
   void IncrementAll() {
-    for (int i = 0; i < kNumRotators; i++) {
-      const float tr = rot[0][i] * rot[2][i] - rot[1][i] * rot[3][i];
-      const float tc = rot[0][i] * rot[3][i] + rot[1][i] * rot[2][i];
-      rot[2][i] = tr;
-      rot[3][i] = tc;
-    }
     for (int c = 0; c < channel.size(); ++c) {
-      for (int i = 0; i < kNumRotators; i++) {
-        const float w = window[i];
-        channel[c].accu[0][i] *= w;
-        channel[c].accu[1][i] *= w;
-        channel[c].accu[2][i] *= w;
-        channel[c].accu[3][i] *= w;
-        channel[c].accu[4][i] *= w;
-        channel[c].accu[5][i] *= w;
-        channel[c].accu[2][i] += channel[c].accu[0][i];
-        channel[c].accu[3][i] += channel[c].accu[1][i];
-        channel[c].accu[4][i] += channel[c].accu[2][i];
-        channel[c].accu[5][i] += channel[c].accu[3][i];
+      for (int i = 0; i < kNumRotators; i++) {  // clang simdifies this.
+	const float w = window[i];
+	for (int k = 2; k < 16; ++k) {
+	  channel[c].accu[k][i] += channel[c].accu[k - 2][i];
+	}
+	const float a = rot[2][i], b = rot[3][i];
+	rot[2][i] = rot[0][i] * a - rot[1][i] * b;
+	rot[3][i] = rot[0][i] * b + rot[1][i] * a;
+	for (int k = 0; k < 16; ++k) channel[c].accu[k][i] *= w;
       }
     }
   }
@@ -193,28 +171,13 @@ struct Rotators {
   }
 };
 
-float BarkFreq(float v) {
-  constexpr float linlogsplit = 0.1;
-  if (v < linlogsplit) {
-    return 20.0 + (v / linlogsplit) * 20.0;  // Linear 20-40 Hz.
-  } else {
-    float normalized_v = (v - linlogsplit) * (1.0 / (1.0 - linlogsplit));
-    return 40.0 * pow(500.0, normalized_v);  // Logarithmic 40-20000 Hz.
-  }
-}
-
 static constexpr int64_t kBlockSize = 1 << 15;
 static const int kHistorySize = (1 << 18);
 static const int kHistoryMask = kHistorySize - 1;
 
 struct RotatorFilterBank {
-  RotatorFilterBank(size_t num_rotators, size_t num_channels, size_t samplerate,
-                    const std::vector<float> &filter_gains) {
-    std::vector<float> freqs(num_rotators);
-    for (size_t i = 0; i < num_rotators; ++i) {
-      freqs[i] = BarkFreq(static_cast<float>(i) / (num_rotators - 1));
-    }
-    rotators_ = new Rotators(num_channels, freqs, filter_gains, samplerate);
+  RotatorFilterBank(size_t num_channels, size_t samplerate) {
+    rotators_ = new Rotators(num_channels, samplerate);
 
     max_delay_ = rotators_->max_delay_;
     QCHECK_LE(max_delay_, kBlockSize);
@@ -334,15 +297,15 @@ void Process(const int output_channels, const double distance_to_interval_ratio,
   constexpr int64_t kNumRotators = 128;
   std::vector<float> freqs(kNumRotators);
   for (size_t i = 0; i < kNumRotators; ++i) {
-    freqs[i] = BarkFreq(static_cast<float>(i) / (kNumRotators - 1));
+    freqs[i] = FreqAve(i);
   }
   std::vector<float> filter_gains(kNumRotators);
   for (size_t i = 0; i < kNumRotators; ++i) {
-    filter_gains[i] = GetFilterGains(i);
+    filter_gains[i] = 1.0;
   }
 
-  RotatorFilterBank rfb(kNumRotators, input_stream.channels(),
-                        input_stream.samplerate(), filter_gains);
+  RotatorFilterBank rfb(input_stream.channels(),
+                        input_stream.samplerate());
 
   std::vector<float> speaker_to_ratio_table;
   speaker_to_ratio_table.reserve(kSubSourcePrecision * (output_channels - 1) +
@@ -415,10 +378,10 @@ void Process(const int output_channels, const double distance_to_interval_ratio,
         float assumed_distance_to_line = stage_size * 1.6;
         float right, center, left;
         rfb.rotators_->GetTriplet(subspeaker_index / (output_channels - 1), rot,
-                                  rfb.rotators_->channel[1].accu[4][rot],
-                                  rfb.rotators_->channel[1].accu[5][rot],
-                                  rfb.rotators_->channel[0].accu[4][rot],
-                                  rfb.rotators_->channel[0].accu[5][rot], right,
+                                  rfb.rotators_->channel[1].accu[14][rot],
+                                  rfb.rotators_->channel[1].accu[15][rot],
+                                  rfb.rotators_->channel[0].accu[14][rot],
+                                  rfb.rotators_->channel[0].accu[15][rot], right,
                                   center, left);
         if (total_in + i >= rfb.max_delay_) {
 #define BINAURAL
